4d455fc9d660a73e975505999f5da3a0ef531f79
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 05:09:35 -0800

    Update CataclysmMod.cs

--- CataclysmMod.cs
+++ CataclysmMod.cs
@@ -4,5 +4,19 @@ namespace CataclysmMod
 {
 	public class CataclysmMod : Mod
 	{
+		public static CataclysmMod Instance { get; private set; }
+
+		public CataclysmMod()
+        {
+			Instance = this;
+
+			Properties = new ModProperties
+			{
+				Autoload = true,
+				AutoloadBackgrounds = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+        }
 	}
 }
