b8edf66ccd38f1d286a52a1d40c3290930f00010
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 08:46:59 -0800

    Lore items give you their effects in piggy banks.

---
+++ Common/Players/LoreItemPlayer.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod.Items.LoreItems;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Common.Players
+{
+    public class LoreItemPlayer : ModPlayer
+    {
+        public override void PostUpdateEquips()
+        {
+            for (int i = 0; i < player.bank.item.Length; i++)
+                if (player.bank.item[i].modItem is LoreItem)
+                {
+                    player.bank.item[i].favorited = true;
+                    ItemLoader.UpdateInventory(player.bank.item[i], player);
+                }
+        }
+    }
+}
