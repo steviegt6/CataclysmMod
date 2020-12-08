c55539abe708472814e5a5d5e90de127ab6b830f
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 19:29:42 -0800

    Fungal Clump

--- CataclysmMod.ILHandler.cs
+++ CataclysmMod.ILHandler.cs
@@ -11,12 +11,14 @@ namespace CataclysmMod
         {
             IL.CalamityMod.World.SmallBiomes.PlaceShrines += ChangeCavernShrineBlocks;
             IL.CalamityMod.World.WorldGenerationMethods.SpecialChest += ChanceCavernShrineChest;
+            IL.CalamityMod.Items.Accessories.FungalClump.UpdateAccessory += RemoveSummonDamageBonus;
         }
 
         internal void UnloadIL()
         {
             IL.CalamityMod.World.SmallBiomes.PlaceShrines -= ChangeCavernShrineBlocks;
             IL.CalamityMod.World.WorldGenerationMethods.SpecialChest -= ChanceCavernShrineChest;
+            IL.CalamityMod.Items.Accessories.FungalClump.UpdateAccessory -= RemoveSummonDamageBonus;
         }
 
         private void ChanceCavernShrineChest(ILContext il)
@@ -82,6 +84,28 @@ namespace CataclysmMod
 
             c.Emit(OpCodes.Pop);
             c.Emit(OpCodes.Ldc_I4, (int)WallID.GrayBrick);
+
+            Logger.Info("[IL] Finished patching!");
+        }
+
+        private void RemoveSummonDamageBonus(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(i => i.MatchLdcR4(10)))
+            {
+                Logger.Warn("[IL] Unable to match ldc.r4 \"10\"!");
+                return;
+            }
+
+            c.Index++;
+
+            c.Emit(OpCodes.Pop);
+            c.Emit(OpCodes.Ldc_I4, 10);
+
+            c.Index++;
+
+            c.RemoveRange(4);
 
             Logger.Info("[IL] Finished patching!");
         }
---
+++ Content/Projectiles/GlobalModifications/FungalClumpProjGlobal.cs
@@ -0,0 +1,41 @@
+﻿using CalamityMod;
+using CalamityMod.Projectiles.Summon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Projectiles.GlobalModifications
+{
+    public class FungalClumpProjGlobal : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+
+        public override bool CloneNewInstances => true;
+
+        public int defDamage = 0;
+
+        public bool firstFrame = true;
+
+        public override void AI(Projectile projectile)
+        {
+            if (projectile.type == ModContent.ProjectileType<FungalClumpMinion>())
+            {
+                if (firstFrame)
+                {
+                    defDamage = projectile.damage;
+                    firstFrame = false;
+                }
+
+                Player player = Main.player[projectile.owner];
+
+                float damageIncrease = 5f * (player.allDamage - 1f);
+                damageIncrease += player.meleeDamage - 1f;
+                damageIncrease += player.rangedDamage - 1f;
+                damageIncrease += player.magicDamage - 1f;
+                damageIncrease += player.minionDamage - 1f;
+                damageIncrease += player.Calamity().throwingDamage - 1f;
+
+                projectile.damage = (int)(damageIncrease + defDamage);
+            }
+        }
+    }
+}
