b0f9f51850a289b08e3ac6b5b80dea98dea070cb
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 19:54:01 -0800

    Allow Anomur Fungus to spawn underground pre-HM

---
+++ Content/NPCs/GlobalModifications/AnomuraFungusGlobal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.NPCs.GlobalModifications
+{
+    public class AnomuraFungusGlobal : GlobalNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (!pool.ContainsKey(NPCID.AnomuraFungus) && spawnInfo.player.ZoneGlowshroom)
+                pool.Add(NPCID.AnomuraFungus, 0.1f);
+        }
+    }
+}
