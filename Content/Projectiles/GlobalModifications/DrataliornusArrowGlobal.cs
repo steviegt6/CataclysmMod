38fe6aeb3cf6e2690e36cb950218e30c0977437c
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 21:16:58 -0800

    Drataliornus arrows can be shot through blocks

---
+++ Content/Projectiles/GlobalModifications/DrataliornusArrowGlobal.cs
@@ -0,0 +1,24 @@
+﻿using CalamityMod.Projectiles.Ranged;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Projectiles.GlobalModifications
+{
+    public class DrataliornusArrowGlobal : GlobalProjectile
+    {
+        public override bool OnTileCollide(Projectile projectile, Vector2 oldVelocity)
+        {
+            if (projectile.type == ModContent.ProjectileType<DrataliornusFlame>())
+                return false;
+
+            return base.OnTileCollide(projectile, oldVelocity);
+        }
+
+        public override void PostAI(Projectile projectile)
+        {
+            if (projectile.type == ModContent.ProjectileType<DrataliornusFlame>())
+                projectile.tileCollide = false;
+        }
+    }
+}
