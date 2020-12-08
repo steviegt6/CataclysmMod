8249bbbe3d7c8dd3a794b0625a98ef7c8885c684
Stevie <xxlennygamerxx@gmail.com>
2020-12-07 20:02:59 -0800

    Spider armor defense buff

---
+++ Content/Items/GlobalModifications/SpiderArmorGlobal.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Items.GlobalModifications
+{
+    public class SpiderArmorGlobal : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            switch (item.type)
+            {
+                case ItemID.SpiderMask:
+                    item.defense += 3;
+                    break;
+
+                case ItemID.SpiderBreastplate:
+                    item.defense += 2;
+                    break;
+
+                case ItemID.SpiderGreaves:
+                    item.defense += 1;
+                    break;
+            }
+        }
+    }
+}
