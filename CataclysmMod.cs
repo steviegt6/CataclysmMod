efb189ef3b442cc83e8c3f59c04ec8208e999c2a
Stevie <xxlennygamerxx@gmail.com>
2020-12-16 14:22:29 -0800

    Throwing Bricks are crafted at work benches instead of anvils now

--- CataclysmMod.cs
+++ CataclysmMod.cs
@@ -1,26 +1,41 @@
+using CalamityMod.Items.Weapons.Rogue;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CataclysmMod
 {
-	public partial class CataclysmMod : Mod
-	{
-		public static CataclysmMod Instance { get; private set; }
+    public partial class CataclysmMod : Mod
+    {
+        public static CataclysmMod Instance { get; private set; }
 
-		public CataclysmMod()
+        public CataclysmMod()
         {
-			Instance = this;
+            Instance = this;
 
-			Properties = new ModProperties
-			{
-				Autoload = true,
-				AutoloadBackgrounds = true,
-				AutoloadGores = true,
-				AutoloadSounds = true
-			};
+            Properties = new ModProperties
+            {
+                Autoload = true,
+                AutoloadBackgrounds = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
         }
 
-		public override void Load() => LoadIL();
+        public override void Load() => LoadIL();
 
-		public override void Unload() => UnloadIL();
+        public override void Unload() => UnloadIL();
+
+        public override void PostAddRecipes()
+        {
+            // Modify Throwing Brick recipe
+            RecipeFinder finder = new RecipeFinder();
+            finder.AddIngredient(ItemID.RedBrick, 5);
+            finder.AddTile(TileID.Anvils);
+            finder.SetResult(ModContent.ItemType<ThrowingBrick>());
+
+            RecipeEditor editor = new RecipeEditor(finder.FindExactRecipe());
+            editor.DeleteTile(TileID.Anvils);
+            editor.AddTile(TileID.WorkBenches);
+        }
     }
 }
