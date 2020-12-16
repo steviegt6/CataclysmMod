365475423e766b2692604adcad1a82015f576ec2
Stevie <xxlennygamerxx@gmail.com>
2020-12-15 22:11:34 -0800

    Basher now has 1.2x scale

---
+++ Content/Items/GlobalModifications/BasherGlobal.cs
@@ -0,0 +1,15 @@
+﻿using CalamityMod.Items.Weapons.Melee;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Items.GlobalModifications
+{
+    public class BasherGlobal : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ModContent.ItemType<Basher>())
+                item.scale = 1.2f;
+        }
+    }
+}
