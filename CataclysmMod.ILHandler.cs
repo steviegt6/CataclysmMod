74d94b334e1c86b195f7c0fba7355827c3453a41
Stevie <xxlennygamerxx@gmail.com>
2020-12-23 18:00:51 -0800

    Steampunker spawns on player

--- CataclysmMod.ILHandler.cs
+++ CataclysmMod.ILHandler.cs
@@ -13,16 +13,18 @@ namespace CataclysmMod
 
             // IL to change Fungal Clump's damage
             IL.CalamityMod.Items.Accessories.FungalClump.UpdateAccessory += FungalClumpDamage.RemoveSummonDamageBonus;
+
+            IL.CalamityMod.World.CalamityWorld.PostUpdate += SteampunkerSpawnIL.ModifySteampunkerSpawn;
         }
 
         internal void UnloadIL()
         {
-            // IL to change the appearance of Cavern Shrines
             IL.CalamityMod.World.SmallBiomes.PlaceShrines -= CavernShrine.ChangeCavernShrineBlocks;
             IL.CalamityMod.World.WorldGenerationMethods.SpecialChest -= CavernShrine.ChangeCavernShrineChest;
 
-            // IL to change Fungal Clump's damage
             IL.CalamityMod.Items.Accessories.FungalClump.UpdateAccessory -= FungalClumpDamage.RemoveSummonDamageBonus;
+
+            IL.CalamityMod.World.CalamityWorld.PostUpdate -= SteampunkerSpawnIL.ModifySteampunkerSpawn;
         }
     }
 }
---
+++ Common/IL/SteampunkerSpawnIL.cs
@@ -0,0 +1,44 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Common.IL
+{
+    public static class SteampunkerSpawnIL
+    {
+        internal static void ModifySteampunkerSpawn(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(x => x.MatchLdcI4(NPCID.Steampunker)))
+            {
+                ModContent.GetInstance<CataclysmMod>().Logger.Warn($"[IL] Unable to match ldc.i4 \"178\"!");
+                return;
+            }
+
+            if (!c.TryGotoNext(x => x.MatchLdloc(0)))
+            {
+                ModContent.GetInstance<CataclysmMod>().Logger.Warn($"[IL] Unable to match ldloc.0!");
+                return;
+            }
+
+            //c.Index++;
+
+            // Remove the entire method call
+            c.RemoveRange(3);
+
+            c.Emit(OpCodes.Ldloc_0); // num
+
+            // Insert our own method call
+            c.EmitDelegate<Action<int>>((whoAmI) =>
+            {
+                Player player = Main.player[whoAmI];
+
+                NPC.NewNPC((int)player.position.X, (int)player.position.Y, NPCID.Steampunker);
+            });
+        }
+    }
+}
