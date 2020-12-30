922ae0d773e9db2920a14825147d4c40f05232dc
Stevie <xxlennygamerxx@gmail.com>
2020-12-30 15:12:09 -0800

    Stuffs

--- Content/Projectiles/GlobalModifications/SummonAIGlobal.cs
+++ Content/Projectiles/GlobalModifications/SummonAIGlobal.cs
@@ -1,13 +1,7 @@
-﻿using CalamityMod;
-using CalamityMod.Buffs.Summon;
-using CalamityMod.CalPlayer;
-using CalamityMod.Projectiles;
-using CalamityMod.Projectiles.Summon;
+﻿using CalamityMod.Projectiles.Summon;
 using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CataclysmMod.Content.Projectiles.GlobalModifications
@@ -18,95 +12,44 @@ namespace CataclysmMod.Content.Projectiles.GlobalModifications
 
         public override bool CloneNewInstances => true;
 
-        public int iceClasperMinionDust = 3;
+        public float savedRotation;
 
-        public static List<int> SummonAIOverhauls = new List<int>()
+        public static Dictionary<int, float> SummonRotationAdjustments = new Dictionary<int, float>()
         {
-            ModContent.ProjectileType<IceClasperMinion>()
+            { ModContent.ProjectileType<IceClasperMinion>(), 0.15f },
+            { ModContent.ProjectileType<PowerfulRaven>(), 0.25f },
+            { ModContent.ProjectileType<HerringMinion>(), 0.25f }
         };
 
-        public override bool PreAI(Projectile projectile) => !HasCustomAI(projectile);
+        public override bool PreAI(Projectile projectile)
+        {
+            if (SummonRotationAdjustments.ContainsKey(projectile.type) || projectile.type == ModContent.ProjectileType<CinderBlossom>())
+                savedRotation = projectile.rotation;
+
+            return base.PreAI(projectile);
+        }
 
         public override void PostAI(Projectile projectile)
         {
-            if (projectile.type == ModContent.ProjectileType<IceClasperMinion>())
-                IceClasperMinionAI(projectile);
-        }
-
-        public void IceClasperMinionAI(Projectile projectile)
-        {
             Player player = Main.player[projectile.owner];
-            CalamityPlayer calPlayer = player.Calamity();
-            CalamityGlobalProjectile calProj = projectile.Calamity();
 
-            // Spawn a "circle" of dust on spawn
-            if (iceClasperMinionDust > 0)
+            if (SummonRotationAdjustments.ContainsKey(projectile.type))
             {
-                calProj.spawnedPlayerMinionDamageValue = player.MinionDamage();
-                calProj.spawnedPlayerMinionProjectileDamageValue = projectile.damage;
+                projectile.rotation = savedRotation;
 
-                for (int i = 0; i < 36; i++)
+                if (projectile.type == ModContent.ProjectileType<PowerfulRaven>())
                 {
-                    Vector2 normalizedVelocity = Vector2.Normalize(projectile.velocity);
-                    Vector2 adjustedVelocity = normalizedVelocity * new Vector2(projectile.width / 2f, projectile.height) * 0.75f;
-                    Vector2 rotation = adjustedVelocity.RotatedBy((i - (36 / 2 - 1)) * ((float)Math.PI * 2f) / 36) + projectile.Center;
-                    Vector2 correctedCenter = rotation - projectile.Center;
-
-                    // Dust ID 67: Unknown.
-                    // There's no field for it in DustID.cs
-                    // Unfortunate.
-                    Dust dust = Dust.NewDustDirect(rotation + correctedCenter, 0, 0, 67, correctedCenter.X * 1.75f, correctedCenter.Y * 1.75f, 100, Scale: 1.1f);
-                    dust.noGravity = true;
-                    dust.velocity = correctedCenter;
+                    projectile.spriteDirection = 1;
+                    LerpToRotation(projectile, SummonRotationAdjustments[projectile.type], 0);
                 }
-
-                iceClasperMinionDust--;
+                else
+                    LerpToRotation(projectile, SummonRotationAdjustments[projectile.type]);
             }
 
-            // Correct damage in case it changes after the minion was spawned
-            if (player.MinionDamage() != calProj.spawnedPlayerMinionDamageValue)
-            {
-                int correctedDamage = (int)(calProj.spawnedPlayerMinionProjectileDamageValue / calProj.spawnedPlayerMinionDamageValue * player.MinionDamage());
-                projectile.damage = correctedDamage;
-            }
-
-            // Handle buff clearing
-            player.AddBuff(ModContent.BuffType<IceClasper>(), 60 * 60);
-
-            if (projectile.type == ModContent.ProjectileType<IceClasperMinion>())
-            {
-                if (player.dead)
-                    calPlayer.iClasper = false;
-
-                if (calPlayer.iClasper)
-                    projectile.timeLeft = 2;
-            }
-
-            // Calamity extension-method that handles charging AI automatially
-            projectile.ChargingMinionAI(1200f, 1500f, 2200f, 150f, 0, 40f, 9f, 4f, new Vector2(0f, -60f), 40f, 9f, true, true);
-
-            /* Visuals */
-
-            // Glow
-            Lighting.AddLight(projectile.Center / 16f, new Vector3(0.05f, 0.15f, 0.2f));
-
-            // Frame animations
-            projectile.frameCounter++;
-
-            if (projectile.frameCounter > 6)
-            {
-                projectile.frame++;
-                projectile.frameCounter = 0;
-            }
-
-            if (projectile.frame > 5)
-                projectile.frame = 0;
-
-            LerpToRotation(projectile);
+            if (projectile.type == ModContent.ProjectileType<CinderBlossom>())
+                projectile.rotation = savedRotation + (MathHelper.ToRadians(1.5f) + (player.velocity.X / player.maxRunSpeed / 5f * player.direction)) * player.direction;
         }
 
-        public static bool HasCustomAI(Projectile projectile) => SummonAIOverhauls.Contains(projectile.type);
-
-        public static void LerpToRotation(Projectile projectile, float amount = 0.15f) => projectile.rotation = projectile.rotation.AngleTowards(projectile.velocity.ToRotation() - MathHelper.PiOver2, amount);
+        public static void LerpToRotation(Projectile projectile, float amount, float adjustment = MathHelper.PiOver2) => projectile.rotation = projectile.rotation.AngleTowards(projectile.velocity.ToRotation() - adjustment, amount);
     }
 }
