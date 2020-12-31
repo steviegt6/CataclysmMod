1d4e69b4f71eda9f646718b059a846c95fd1d385
Stevie <xxlennygamerxx@gmail.com>
2020-12-30 17:33:41 -0800

    Abyss Mines from the Slime God all explode after the Slime God has been defeated

---
+++ Content/Projectiles/GlobalModifications/SlimeGodMineGlobal.cs
@@ -0,0 +1,17 @@
+﻿using CalamityMod.NPCs.SlimeGod;
+using CalamityMod.Projectiles.Boss;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Projectiles.GlobalModifications
+{
+    public class SlimeGodMineGlobal : GlobalProjectile
+    {
+        public override void PostAI(Projectile projectile)
+        {
+            if (projectile.type == ModContent.ProjectileType<AbyssMine>())
+                if (!NPC.AnyNPCs(ModContent.NPCType<SlimeGodCore>()) && !NPC.AnyNPCs(ModContent.NPCType<SlimeGod>()) && !NPC.AnyNPCs(ModContent.NPCType<SlimeGodSplit>()) && !NPC.AnyNPCs(ModContent.NPCType<SlimeGodRun>()) && !NPC.AnyNPCs(ModContent.NPCType<SlimeGodRunSplit>()))
+                    projectile.Kill();
+        }
+    }
+}
