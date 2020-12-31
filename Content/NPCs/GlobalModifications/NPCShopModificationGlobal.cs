e1f902edc380a4717127982c70fcd0f7b906b7b8
Stevie <xxlennygamerxx@gmail.com>
2020-12-30 17:02:48 -0800

    The Wizard now sells Guide Voodoo Dolls

---
+++ Content/NPCs/GlobalModifications/NPCShopModificationGlobal.cs
@@ -0,0 +1,22 @@
+﻿using CalamityMod.NPCs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.NPCs.GlobalModifications
+{
+    public class NPCShopModificationGlobal : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            switch (type)
+            {
+                case NPCID.Wizard:
+                    SetupWizardShop(shop, ref nextSlot);
+                    break;
+            }
+        }
+
+        public void SetupWizardShop(Chest shop, ref int nextSlot) => CalamityGlobalTownNPC.SetShopItem(ref shop, ref nextSlot, ItemID.GuideVoodooDoll, Main.hardMode, Item.sellPrice(gold: 20));
+    }
+}
