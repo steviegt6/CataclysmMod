936d03b7285a55a053edc2f7e65bdbf7ff1f972e
Stevie <xxlennygamerxx@gmail.com>
2020-12-30 17:20:17 -0800

    Removed Death mode changes to the Torrential Tear

--- CataclysmMod.ILHandler.cs
+++ CataclysmMod.ILHandler.cs
@@ -1,4 +1,5 @@
-﻿using CataclysmMod.Common.IL;
+﻿using CataclysmMod.Common.Detours;
+using CataclysmMod.Common.IL;
 using Terraria.ModLoader;
 
 namespace CataclysmMod
@@ -15,6 +16,10 @@ namespace CataclysmMod
             IL.CalamityMod.Items.Accessories.FungalClump.UpdateAccessory += FungalClumpDamage.RemoveSummonDamageBonus;
 
             IL.CalamityMod.World.CalamityWorld.PostUpdate += SteampunkerSpawnIL.ModifySteampunkerSpawn;
+
+            /* Detours */
+            // Remove annoying Death mode changes to the Torrential Tear
+            On.CalamityMod.Items.Tools.ClimateChange.TorrentialTear.UseItem += TorrentialTearDetour.RemoveDeathModeCrap;
         }
 
         internal void UnloadIL()
---
+++ Common/Detours/TorrentialTearDetour.cs
@@ -0,0 +1,21 @@
+﻿using CalamityMod;
+using CalamityMod.Items.Tools.ClimateChange;
+using Terraria;
+
+namespace CataclysmMod.Common.Detours
+{
+    public static class TorrentialTearDetour
+    {
+        public static bool RemoveDeathModeCrap(On.CalamityMod.Items.Tools.ClimateChange.TorrentialTear.orig_UseItem orig, TorrentialTear self, object player)
+        {
+            if (!Main.raining)
+                CalamityUtils.StartRain(torrentialTear: true);
+            else
+                Main.raining = false;
+
+            CalamityNetcode.SyncWorld();
+
+            return true;
+        }
+    }
+}
