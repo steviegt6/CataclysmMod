a5e8b62f89ce0ac0755df6efe33f4b919643d86c
Stevie <xxlennygamerxx@gmail.com>
2021-01-25 18:20:34 -0800

    Slime God inflicts Slimed instead of Distorted

---
+++ Common/Detours/SlimeGodDistortionDetour.cs
@@ -0,0 +1,23 @@
+﻿using CataclysmMod.Common.Configs;
+using Terraria;
+using Terraria.ID;
+
+namespace CataclysmMod.Common.Detours
+{
+    public class SlimeGodDistortionDetour : Detour
+    {
+        public override string DictKey => "On.CalamityMod.NPCs.SlimeGod.SlimeGodCore.OnHitPlayer";
+
+        public override void Load() => On.CalamityMod.NPCs.SlimeGod.SlimeGodCore.OnHitPlayer += NewOnHitBuff;
+
+        public override void Unload() => On.CalamityMod.NPCs.SlimeGod.SlimeGodCore.OnHitPlayer -= NewOnHitBuff;
+
+        private void NewOnHitBuff(On.CalamityMod.NPCs.SlimeGod.SlimeGodCore.orig_OnHitPlayer orig, CalamityMod.NPCs.SlimeGod.SlimeGodCore self, object player, int damage, bool crit)
+        {
+            if (player is Player && CalamityChangesConfig.Instance.slimeGodSlimedDebuff)
+                (player as Player).AddBuff(BuffID.Slimed, 60 * 2);
+            else
+                orig(self, player, damage, crit);
+        }
+    }
+}
