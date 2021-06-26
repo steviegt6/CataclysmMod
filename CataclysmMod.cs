eb7d157ab6f02d4faa60c66d1cb30beae593b733
Stevie <xxlennygamerxx@gmail.com>
2021-06-26 16:02:02 -0700

    obliterate

--- CataclysmMod.cs
+++ CataclysmMod.cs
@@ -10,45 +10,5 @@ namespace CataclysmMod
 {
     public class CataclysmMod : Mod
     {
-        public static Version ExpectedCalamityVersion => new Version(1, 4, 5, 7);
-
-        public static CataclysmMod Instance { get; private set; }
-
-        public CataclysmMod()
-        {
-            Instance = this;
-
-            Properties = new ModProperties
-            {
-                Autoload = true,
-                AutoloadBackgrounds = true,
-                AutoloadGores = true,
-                AutoloadSounds = true
-            };
-        }
-
-        public override void Load()
-        {
-            CalamityVersionException.ThrowErrorOnIncorrectVersion(ExpectedCalamityVersion);
-
-            ApplicableManager.Load();
-            SummonRotationAdjustmentsGlobalProj.Initialize();
-            AbyssalMinesExplosionGlobalProj.Initialize();
-            ArmorSetDatabase.Initialize();
-            TooltipModificationsItem.Initialize();
-        }
-
-        public override void Unload()
-        {
-            ApplicableManager.Unload();
-            CataclysmConfig.Instance = null;
-            Instance = null;
-        }
-
-        public override void AddRecipes() => RecipeHandler.AddRecipes();
-
-        public override void PostAddRecipes() => RecipeHandler.ModifyRecipes();
-
-        public override void AddRecipeGroups() => RecipeHandler.AddRecipeGroups();
     }
 }
