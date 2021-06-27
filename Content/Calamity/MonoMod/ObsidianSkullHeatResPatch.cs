95e9695b9da5ff26a6e4afb22d617491310e0c0d
Stevie <xxlennygamerxx@gmail.com>
2021-06-26 23:10:27 -0700

    patches

---
+++ Content/Calamity/MonoMod/ObsidianSkullHeatResPatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using CalamityMod.CalPlayer;
+using CataclysmMod.Common.ModCompatibility;
+using CataclysmMod.Common.Utilities;
+using CataclysmMod.Content.Calamity.GlobalModifications.ModPlayers;
+using CataclysmMod.Content.Default.MonoMod;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria;
+
+namespace CataclysmMod.Content.Calamity.MonoMod
+{
+    [ModDependency("CalamityMod")]
+    public class ObsidianSkullHeatResPatch : MonoModPatcher<string>
+    {
+        public override MethodInfo Method => typeof(CalamityPlayerMiscEffects).GetCachedMethod("MiscEffects");
+
+        public override string ModderMethod => nameof(AddObsidianSkullCraftingTreeHeatImmunity);
+
+        public static void AddObsidianSkullCraftingTreeHeatImmunity(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (!c.TryGotoNext(i => i.MatchLdfld(typeof(CalamityPlayer).GetField("externalHeatImmunity",
+                BindingFlags.Instance | BindingFlags.Public))))
+            {
+                throw new Exception(
+                    "Patch failure: ldfld -> CalamityMod.CalPlayer.CalamityPlayer::externalHeatImmunity");
+            }
+
+            c.Index += 2;
+
+            c.Emit(OpCodes.Stloc, 17);
+            c.Emit(OpCodes.Ldarg_0);
+
+            // ReSharper disable once RedundantAssignment
+            c.EmitDelegate<Action<bool, Player>>((funnyLavaHeatRes, player) =>
+            {
+                if (player.GetModPlayer<CalamityCataclysmPlayer>().ObsidianSkullHeatRes)
+                    // ReSharper disable once RedundantAssignment
+                    funnyLavaHeatRes = true;
+            });
+        }
+    }
+}
---
+++ Content/Calamity/MonoMod/ShrineBlocksPatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using CalamityMod.World;
+using CataclysmMod.Common.Utilities;
+using CataclysmMod.Content.Default.MonoMod;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria.ID;
+
+namespace CataclysmMod.Content.Calamity.MonoMod
+{
+    public class ShrineBlocksPatch : MonoModPatcher<string>
+    {
+        public override MethodInfo Method => typeof(SmallBiomes).GetCachedMethod(nameof(SmallBiomes.PlaceShrines));
+
+        public override string ModderMethod => nameof(ChangeCavernShrineBlocks);
+
+        public static void ChangeCavernShrineBlocks(ILContext il)
+        {
+            void DoPatch(ILCursor cursor)
+            {
+                /* Match the first number in the call for WorldGenerationMethods.SpecialHut for cavern shrines (75)
+                 * // WorldGenerationMethods.SpecialHut(75, 56, 20, 2, num8, num10);
+                 * IL_0225: ldc.i4.s 75 // Cavern shrine tile
+                 * IL_0227: ldc.i4.s 56 // Cavern shrine tile
+                 * IL_0229: ldc.i4.s 20 // Cavern shrine tile
+                 * IL_022b: ldc.i4.2
+                 * IL_022c: ldloc.s 13
+                 * IL_022e: ldloc.s 15
+                 */
+                if (!cursor.TryGotoNext(i => i.MatchLdcI4(75)))
+                {
+                    throw new Exception("Patch failure: ldc.i4.s -> 75");
+                }
+
+                cursor.Index++;
+
+                // Pop the normal values and replace them with the IDs for stone bricks and stone
+                cursor.Emit(OpCodes.Pop);
+                cursor.Emit(OpCodes.Ldc_I4, TileID.GrayBrick);
+
+                cursor.Index++;
+
+                cursor.Emit(OpCodes.Pop);
+                cursor.Emit(OpCodes.Ldc_I4, TileID.Stone);
+
+                cursor.Index++;
+
+                cursor.Emit(OpCodes.Pop);
+                cursor.Emit(OpCodes.Ldc_I4, (int) WallID.GrayBrick);
+            }
+
+            ILCursor c = new ILCursor(il);
+
+            DoPatch(c);
+            DoPatch(c);
+        }
+    }
+}
