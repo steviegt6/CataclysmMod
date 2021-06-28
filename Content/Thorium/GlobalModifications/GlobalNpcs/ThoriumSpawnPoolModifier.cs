3b545efc034ccb259c26bf9b5949984ad23929d0
Stevie <xxlennygamerxx@gmail.com>
2021-06-27 21:12:49 -0700

    globee deez nuts

---
+++ Content/Thorium/GlobalModifications/GlobalNpcs/ThoriumSpawnPoolModifier.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using CataclysmMod.Common.ModCompatibility;
+using CataclysmMod.Content.Default.GlobalModifications;
+using Terraria.ModLoader;
+using ThoriumMod.NPCs.Depths;
+
+namespace CataclysmMod.Content.Thorium.GlobalModifications.GlobalNpcs
+{
+    [ModDependency("ThoriumMod")]
+    public class ThoriumSpawnPoolModifier : CataclysmGlobalNpc
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (pool.ContainsKey(ModContent.NPCType<Globee>())) 
+                pool[ModContent.NPCType<Globee>()] *= 2f;
+        }
+    }
+}
