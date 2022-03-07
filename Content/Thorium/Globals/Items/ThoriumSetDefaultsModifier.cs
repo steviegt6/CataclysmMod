2e439ea9126b925118e5a05db6bfd09bd96341a2
Steviegt6 <xxlennygamerxx@gmail.com>
2022-03-06 22:35:55 -0800

    Reimplement the funny Thorium stuff.

---
+++ Content/Thorium/Globals/Items/ThoriumSetDefaultsModifier.cs
@@ -0,0 +1,24 @@
+﻿#region License
+// Copyright (C) 2022 Tomat and Contributors, MIT License
+#endregion
+
+using CataclysmMod.Common.Addons;
+using CataclysmMod.Core.Loading;
+using Terraria;
+using Terraria.ModLoader;
+using ThoriumMod.Items.Placeable;
+
+namespace CataclysmMod.Content.Thorium.Globals.Items
+{
+    [AddonContent(typeof(ThoriumModAddon))]
+    public class ThoriumSetDefaultsModifier : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ModContent.ItemType<BlackStainedGlass>() ||
+                item.type == ModContent.ItemType<CyanStainedGlass>() ||
+                item.type == ModContent.ItemType<PinkStainedGlass>())
+                item.value = Utils.Clamp(item.value, Item.sellPrice(silver: 5), int.MaxValue);
+        }
+    }
+}
---
+++ Content/Thorium/Globals/NPCs/ThoriumSpawnPoolModifier.cs
@@ -0,0 +1,22 @@
+﻿#region License
+// Copyright (C) 2022 Tomat and Contributors, MIT License
+#endregion
+
+using System.Collections.Generic;
+using CataclysmMod.Common.Addons;
+using CataclysmMod.Core.Loading;
+using Terraria.ModLoader;
+using ThoriumMod.NPCs.Depths;
+
+namespace CataclysmMod.Content.Thorium.Globals.NPCs
+{
+    [AddonContent(typeof(ThoriumModAddon))]
+    public class ThoriumSpawnPoolModifier : GlobalNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (pool.ContainsKey(ModContent.NPCType<Globee>())) 
+                pool[ModContent.NPCType<Globee>()] *= 2f;
+        }
+    }
+}
