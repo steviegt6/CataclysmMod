5240ab3a9948c824691c8a2506c3bf3859904865
Steviegt6 <xxlennygamerxx@gmail.com>
2022-03-06 22:05:02 -0800

    Reimplement Meance vs. Umbrella

---
+++ Content/Split/Globals/Projectiles/MenaceUmbrellaGlobalProjectile.cs
@@ -0,0 +1,31 @@
+﻿#region License
+// Copyright (C) 2022 Tomat and Contributors, MIT License
+#endregion
+
+using System.Linq;
+using CataclysmMod.Common.Addons;
+using CataclysmMod.Content.Split.Projectiles;
+using CataclysmMod.Core.Loading;
+using Split.Projectiles.Hostile.Bosses.Menace;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Split.Globals.Projectiles
+{
+    [AddonContent(typeof(SplitAddon))]
+    public class MenaceUmbrellaGlobalProjectile : GlobalProjectile
+    {
+        public override void AI(Projectile projectile)
+        {
+            base.AI(projectile);
+
+            bool MenaceRainProjectile(int type) => type == ModContent.ProjectileType<MenacingKnife>() ||
+                                                   type == ModContent.ProjectileType<IceBlock>();
+
+            foreach (Projectile proj in Main.projectile.Where(x =>
+                         x.active && x.type.Equals(ModContent.ProjectileType<PlayerUmbrellaProjectile>())))
+                if (MenaceRainProjectile(projectile.type) && proj.Hitbox.Intersects(projectile.Hitbox))
+                    projectile.Kill();
+        }
+    }
+}
---
+++ Content/Split/Projectiles/PlayerUmbrellaProjectile.cs
@@ -0,0 +1,39 @@
+﻿#region License
+// Copyright (C) 2022 Tomat and Contributors, MIT License
+#endregion
+
+using CataclysmMod.Common.Addons;
+using CataclysmMod.Core.Loading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CataclysmMod.Content.Split.Projectiles
+{
+    [AddonContent(typeof(SplitAddon))]
+    public class PlayerUmbrellaProjectile : ModProjectile
+    {
+        public override string Texture => "Terraria/Projectile_1";
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor) => false;
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            projectile.Size = new Vector2(46f, 20f);
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            Player player = Main.player[projectile.owner];
+            projectile.position = player.position - new Vector2(6f, 30f);
+
+            if (player.direction == -1)
+                projectile.position.X -= 14f;
+        }
+    }
+}
